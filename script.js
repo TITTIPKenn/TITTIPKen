62daab9435172625aa2eddba6f2e9f8bec18ecda
TITTIPKen <5prm48t84f@privaterelay.appleid.com>
2026-08-01 23:05:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -435,7 +435,7 @@ Subtotal: Rp${subtotal}
 Diskon (${percent}%): -Rp${discount}
 TOTAL: Rp${total}`;
 
-  window.open("https://wa.me/6289633016767?text=" + encodeURIComponent(msg));
+  window.open("https://chat.whatsapp.com/JU5urLr0GA0KjLzZd69ZMQ?s=cl&p=i&mlu=4" + encodeURIComponent(msg));
 
 }
 
